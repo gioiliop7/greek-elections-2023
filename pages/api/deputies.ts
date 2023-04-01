2212bab7147f4c56e719c8011ced2ca519ee9d97
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-04-01 17:43:54 +0300

    vouleutes endpoint

---
+++ pages/api/deputies.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+interface ErrorResponse {
+  error: string;
+  status?: number;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any | ErrorResponse>
+) {
+  if (req.method !== "GET") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
+  }
+
+  const url = "https://ekloges.ypes.gr/current/dyn/v/300.js";
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error: unknown) {
+    if (typeof error === "object" && error !== null && "status" in error) {
+      const { status, message } = error as { status: number; message: string };
+      res.status(status || 500).json({ error: message });
+    } else {
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  }
+}
