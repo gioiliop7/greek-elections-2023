f843335441afe21a5aa808c85fcfc9c61034a748
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-04-29 18:55:59 +0300

    Create party and ep routes

---
+++ pages/api/party.ts
@@ -0,0 +1,40 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  error: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method !== "POST") {
+    res.status(405).json({ error: "Method Not Allowed" });
+    return;
+  }
+
+  const { id } = req.body;
+
+  if (id === undefined) {
+    return res.status(400).json({ error: "Missing ID parameter" });
+  }
+
+  const idNumber = parseInt(id, 10);
+  if (isNaN(idNumber)) {
+    return res.status(400).json({ error: "Invalid ID parameter" });
+  }
+
+  const url = `https://ekloges.ypes.gr/current/dyn/v/party_${idNumber}.js`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      return res.status(404).json({ error: "Not Found" });
+    }
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+}
---
+++ pages/ep/[id].tsx
@@ -0,0 +1,51 @@
+import { GetServerSideProps } from "next";
+
+interface Props {
+  data: any;
+}
+
+export default function Ep({ data }: Props) {
+  return (
+    <div>
+      <h1>Data:</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async ({
+  query,
+}) => {
+  const id = query.id;
+
+  if (!id) {
+    return {
+      notFound: true,
+    };
+  }
+
+  if (isNaN(Number(id))) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const dev = process.env.NODE_ENV !== "production";
+  const server = dev ? "http://localhost:3000" : process.env.EKLOGES_PUBLIC_URL;
+
+  const response = await fetch(`${server}/api/ep`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ id }),
+  });
+
+  const data = await response.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
