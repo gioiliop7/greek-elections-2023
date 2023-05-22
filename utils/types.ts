dd5d8daf3c7f16b88d294c455ad6726984deaac1
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-05-22 21:42:32 +0300

    new types for party names

--- utils/types.ts
+++ utils/types.ts
@@ -51,6 +51,35 @@ export type PartyNames = {
   106: string;
   108: string;
   122: string;
-}
+  131: string;
+  123: string;
+  135: string;
+  60: string;
+  128: string;
+  28: string;
+  129: string;
+  44: string;
+  140: string;
+  15: string;
+  138: string;
+  130: string;
+  142: string;
+  141: string;
+  134: string;
+  125: string;
+  113: string;
+  54: string;
+  143: string;
+  137: string;
+  85: string;
+  133: string;
+  10: string;
+  66: string;
+  139: string;
+  11: string;
+  136: string;
+  132: string;
+  20: string;
+};
 
 export type RequestType = "stats" | "full";
