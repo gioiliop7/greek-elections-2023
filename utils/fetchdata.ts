d1aef867463d10987c0b0d842ebc70760822ce19
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-06-11 18:15:19 +0300

    debug

--- utils/fetchdata.ts
+++ utils/fetchdata.ts
@@ -1,5 +1,6 @@
 const dev = process.env.NODE_ENV !== "production";
 const server = dev ? "http://localhost:3000" : process.env.EKLOGES_PUBLIC_URL;
+console.log(server)
 import { ElectionData } from "./types";
 
 export const fetchData = async (ep = 99999): Promise<ElectionData> => {
