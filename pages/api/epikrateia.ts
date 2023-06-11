e21c65139d39e5bb93efb1e6040e64e9cb953a51
Giorgos Iliopoulos <gio.giorgaras@gmail.com>
2023-06-11 18:39:56 +0300

    all the headers

--- pages/api/epikrateia.ts
+++ pages/api/epikrateia.ts
@@ -39,7 +39,18 @@ export default async function handler(
         accept: "application/json, text/plain, */*",
         "accept-language": "el-GR,el;q=0.9,en;q=0.8",
         "cache-control": "no-cache",
+        cookie: "_ga=GA1.2.223126820.1679741516; .Elections.Culture=el",
+        pragma: "no-cache",
         referer: "https://ekloges.ypes.gr/current/v/home/",
+        "sec-ch-ua":
+          '"Google Chrome";v="111", "Not(A:Brand";v="8", "Chromium";v="111"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36",
       },
     };
     const response = await fetch(url, requestOptions);
